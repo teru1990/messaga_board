f40b34a30ac72eaafc01dd43aff76b29a8ff9053
teruhito <teru0516part@yahoo.co.jp>
2020-07-19 01:03:02 +0900

    Add NewServlet.java

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,15 +7,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "messages")
 public class Message {
     @Id
     @Column( name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "update_at",  nullable = false)
+    private Timestamp update_at;
 
     public Integer getId() {
         return id;
@@ -41,6 +61,14 @@ public class Message {
         this.content = content;
     }
 
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
     public Timestamp getUpdate_at() {
         return update_at;
     }
@@ -49,12 +77,4 @@ public class Message {
         this.update_at = update_at;
     }
 
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
-
-    @Column(name = "content", length = 255, nullable = false)
-    private String content;
-
-    @Column(name = "update_at", length = 255, nullable = false)
-    private Timestamp update_at;
-}
+  }
